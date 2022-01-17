f40162b4cd923a2b5d4b5d9e1c596b1acb8870cb
alx-nder <tyreekalx@gmail.com>
2022-01-17 13:16:42 -0500

    algo done ish

--- recommend.py
+++ recommend.py
@@ -1,59 +1,77 @@
 import random
 
-from matplotlib.pyplot import bar_label
+from numpy import true_divide
 
-def highofhigh(mass,action):
-    max=mass[0][-1]
+def highofhigh(firstnestedlist,action):
+    
+    if type(firstnestedlist[0]) != type([list]) and type(firstnestedlist[-1]== type(int)):
+        print(firstnestedlist)
+        return firstnestedlist
+    
+    max=firstnestedlist[0][-1]
     maxi=0
-    for x in range(0, len(mass)):
-        if type(mass[x]) == "int":
+
+    for x in range(0, len(firstnestedlist)):
+        #is this necessary? it checks if nested
+        if type(firstnestedlist[x]) != list and type(firstnestedlist[-1]== int):
             break
-        if mass[x][-1] > max:
-            max=mass[x][-1]
+
+        elif firstnestedlist[x][-1] > max:
+            max=firstnestedlist[x][-1]
             maxi=x
     #running action to change values
-    mass[maxi][-1]+=action
-    newlist=mass[maxi]
-    return newlist  
+    firstnestedlist[maxi][-1]+=action
+    
+    #or say if index0= list run func
+    #newlist=mass[maxi]
+    #if type(newlist[x]) != "list":
+    #    return newlist
+    #print(firstnestedlist)
+    secondnestedlist=firstnestedlist[maxi]
+    highofhigh(secondnestedlist, action)
 
+ 
 #prices min, max, count
-p1=[7,10,0]
-p2=[3,6,0]
-p3=[1,2,0]
+p1=[7,10,2]
+p2=[3,6,1]
+p3=[1,2,3]
 
-a1=['va',0]
+a1=['va',2]
 a2=['md',1]
-a3=['ja',0]
+a3=['ja',3]
 
 price=[p1,p2,p3,0]
 address=[a1,a2,a3,1]
 mass=[price,address]
 
-def epsilon1(mass,action):
-  print("Hello from a function")
+def randomiz(nth,action):
+    #prevent choosing last
+    if type(nth[0]) != list and type(nth[-1]== int):
+        print(nth)
+        return nth
+    #check if first    
+    ###something weird happened here 
+    elif type(nth[-1])!= int:
+        first=random.choice(nth)
+        #first is a list
+        first[-1]+=action
+        randomiz(first,action)
+    #recursive
+    else:    
+        first = random.randrange(0,len(nth)-1)
+        nth[first][-1]+=action
+        randomiz(nth[first],action)
+
+      
+def epsilon1(mass,action):  
   n1= random.random()
   n2= random.random()
   print(n1, n2)
   if n1 > n2:
-      #random from the nested lists
-      x=random.choice(mass)
-      ######=random.c(x)
-      #all except last ie count
-      #running action to change values
-      xl=0
-      xl=x[-1]
-      ####yl=0
-      ####yl=y[-1]
-      xl+= action
-      ###yl+= action
-
-      
-      #print(y,type(y[-1]) )
+      randomiz(mass,action)
   else:
-    newlist = highofhigh(mass,action)
-    print(newlist)
-
-
+      highofhigh(mass,action)
+      
 
 epsilon1(mass,1)
 
