7dcf389ed157c18ada7a0d123bb78779bb5d1495
alx-nder <tyreekalx@gmail.com>
2022-01-31 13:17:15 -0500

    smells fixed plus comments added

--- epsilon1.py
+++ epsilon1.py
@@ -1,69 +1,71 @@
 #!C:\Program Files\Python310\python.exe
 print("content-type: text/html\n\n" )
 
-
 import sys
 sys.path.append(r'''C:\Users\tyree\AppData\Roaming\Python\Python310\site-packages''')
 
+#module to generate random numbers
 import random
+
+#module to manipulate sql database
 import mysql.connector
 
+#connecting to database
 db=mysql.connector.connect(
     host="localhost",
     user="root",
     database="virttour"
 ) 
 mycursor= db.cursor()
-
-
-def highofhigh(firstnestedlist,action):
-    
-    if type(firstnestedlist[0]) != type([list]) and type(firstnestedlist[-1]== type(int)):
-    
-        print(firstnestedlist[0])
-        return firstnestedlist[0]
-    
-    max=firstnestedlist[0][-1]
-    maxi=0
-
-    for x in range(0, len(firstnestedlist)):
-        if type(firstnestedlist[x]) != list and type(firstnestedlist[-1]== int):
-            break
-        elif firstnestedlist[x][-1] > max:
-            max=firstnestedlist[x][-1]
-            maxi=x
-    #running action to change values
-    firstnestedlist[maxi][-1]+=action
-    
-    #or say if index0= list run func
-    #newlist=mass[maxi]
-    #if type(newlist[x]) != "list":
-    #    return newlist
-    #print(firstnestedlist)
-    secondnestedlist=firstnestedlist[maxi]
-    highofhigh(secondnestedlist, action)
-
  
-#prices min, max, count
+#prices min, max, interaction count
 p1=[10,10,2]
 p2=[10,6,1]
 p3=[10,2,3]
 
-a1=["va",2]
-a2=["md",1]
-a3=["ja",3]
+#addresses codes, interaction count
+a1=['va',2]
+a2=['md',1]
+a3=['ja',3]
 
+#list of prices and an interaction count
 price=[p1,p2,p3,0]
-address=[a1,a2,a3,1]
-mass=[price,address]
 
+#list of addresses and an interaction count
+address=[a1,a2,a3,1]
+grand_list=[price,address]
+
+#this function takes a list and integer as parameters, checks to see what item in the list has the highest interaction count then returns/ prints that item.
+def highofhigh(nested_list,action):
+#base case -  if the list argument does not contain a nested list, return the first element of the current list and end.    
+    if type(nested_list[0]) != type([list]) and type(nested_list[-1]== type(int)):
+        print(nested_list[0])
+        return nested_list[0]
+    
+    interaction_count=nested_list[0][-1]
+    max_index=0
+    
+#traverses list to find the highest nested interaction count
+    for x in range(0, len(nested_list)):
+        if type(nested_list[x]) != list and type(nested_list[-1]== int):
+            break
+        elif nested_list[x][-1] > interaction_count:
+            interaction_count=nested_list[x][-1]
+            max_index=x
+    #running action to update interaction count
+    nested_list[max_index][-1]+=action
+    
+    secondnestedlist=nested_list[max_index]
+    highofhigh(secondnestedlist, action)
+
+#function to randomly choose an item to display from nested lists
 def randomiz(nth,action):
-    #prevent choosing last
+    #prevent choosing last element - interaction count
     if type(nth[0]) != list and type(nth[-1]== int):
         print(nth[0])
         return nth[0]
-    #check if first    
-    ###something weird happened here 
+    
+    #check if the list argument is the grand_list, which has no interaction count variable   
     elif type(nth[-1])!= int:
         first=random.choice(nth)
         #first is a list
@@ -75,19 +77,14 @@ def randomiz(nth,action):
         nth[first][-1]+=action
         randomiz(nth[first],action)
 
-      
-def epsilon1(mass,action):  
+def epsilon1(grand_list,action):  
+#coin toss to choose recommendation form random or preferenced
   n1= random.uniform(0,1)
   n2= random.uniform(0,1)
-  #print(n1, n2)
+  
   if n1 > n2:
-      randomiz(mass,action)
+      randomiz(grand_list,action)
   else:
-      highofhigh(mass,action)
+      highofhigh(grand_list,action)
       
-epsilon1(mass,1)
-
-# rec=epsilon1(mass,1)
-# mycursor.execute("select * From listings where price = 'rec' OR add = 'rec'")
-# for x in mycursor:
-#     print (x)
+epsilon1(grand_list,1)
