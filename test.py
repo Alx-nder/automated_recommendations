5daf25d8367fce64bc87448fdeeb66e31ef0e8c7
alx-nder <tyreekalx@gmail.com>
2022-03-14 21:37:59 -0500

    interaction location complete

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 #!C:\Program Files\Python310\python.exe
 print("content-type: text/html;\n\n" )
 import cgi
+from code import interact
 import pandas as pd
 import mysql.connector
 
@@ -40,9 +41,8 @@ db_to_list(price,"price")
 print(location,"\n\n",price)
 
 
-query1="select * from user_pref"
-
 # pandas method 
+query1="select * from user_pref"
 pref_data=pd.read_sql(query1,db)
 pref_data.set_index("username", inplace=True)
 print(pref_data)
@@ -50,9 +50,17 @@ print(pref_data)
        
 for i in range(0,len(pref_data.columns)):
     if "loc" in pref_data.columns[i]:
-        
+
         location[i-3].append(pref_data[pref_data.columns[i]]['t@y.com'])
     else:
         price[i].append(pref_data[pref_data.columns[i]]['t@y.com'])    
+
+print(location,"\n\n",price)
+
+location.append(0)
+
+# use len()-1 because last element is a int-interaction
+for i in range(0,len(location)-1):
+    location[-1]+=location[i][-1]
 
 print(location,"\n\n",price)
