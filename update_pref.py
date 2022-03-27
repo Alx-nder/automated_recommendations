e0a7e623352849d944c0b8fa5b46f351c933f38b
alx-nder <tyreekalx@gmail.com>
2022-03-27 11:05:32 -0500

    recieves data and updates

--- update_pref.py
+++ update_pref.py
@@ -9,6 +9,7 @@ sys.path.append(r'C:\Users\tyree\AppData\Roaming\Python\Python310\site-packages'
 import json
 import cgi
 import mysql.connector
+import pandas as pd
 
 # connecting to database
 db=mysql.connector.connect(
@@ -17,19 +18,33 @@ db=mysql.connector.connect(
     database="virttour"
 )
 
-## accept ajax body
+# ## accept ajax body
 form=cgi.FieldStorage()
-username=json.loads(form.getvalue("message_py"))
 
-# cursor = db.cursor()
+# loads json the obj sent from js 
+js_postdata=json.loads(form.getvalue("message_py"))
+location_pref=js_postdata["card_location"]
+current_user=js_postdata["username"]
 
-# query=f"update user_pref set {} where username='guest'"
-# cursor.execute(query)
+
+query1="select * from user_pref"
+pref_data=pd.read_sql(query1,db)
+pref_data.set_index("username", inplace=True)
+
+### a single record located at [col][row]
+interaction=pref_data[location_pref][current_user]
+
+### updating the interaction
+
+cursor = db.cursor()
+##using backquotes in the event of non-conformant field names
+query=f"update user_pref set `{location_pref}`={interaction+1} where username='{current_user}'"
+cursor.execute(query)
 
 
-# query=f"select * from user_pref where username='guest'"
-# cursor.execute(query)
+tquery=f"select * from user_pref where username='guest'"
+cursor.execute(tquery)
 
-# records =json.dumps(cursor.fetchall()[0])
+records =json.dumps(cursor.fetchall())
 
-print(username["card_location"])
+print(records)
