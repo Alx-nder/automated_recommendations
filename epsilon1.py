122580062ed3ff5bf41859a4c95698ef1fc6e4bf
alx-nder <tyreekalx@gmail.com>
2022-03-28 22:57:38 -0500

    highest interaction algo bug fix

--- epsilon1.py
+++ epsilon1.py
@@ -39,18 +39,20 @@ def randomiz(nth):
 def highofhigh(nested_list):
 #base case -  if the list argument does not contain a nested list, return the first element of the current list and end.    
     if type(nested_list) is list:
-        ### use the -1 as escape
-        if nested_list[-1]==-1:
-            nested_list.pop()
-        #we assume the first element has the highest interaction count 
-        interaction_count=nested_list[0][-1]
+
+        #we assume 0 is the highest interaction count 
+        interaction_count=0
         #the position of the list with the higher/highest interaction count
         max_index=0
+
+        ### remove the last element, the interaction, because we cannot traverse an int like a list
+        nested_list.pop()
+        
         
     #traverses list to find the highest nested interaction count
         for x in range(0, len(nested_list)):
             # end of nested
-            if type(nested_list[x]) != list and type(nested_list[-1]== int):
+            if type(nested_list[x]) != list:
                 break
             elif nested_list[x][-1] > interaction_count:
                 interaction_count=nested_list[x][-1]
@@ -59,7 +61,7 @@ def highofhigh(nested_list):
         secondnestedlist=nested_list[max_index] #declaring the list with highest interaction count
         return highofhigh(secondnestedlist)
     else:
-        return(nested_list)
+        return nested_list
 
 
 def epsilon1(grand_list):  
